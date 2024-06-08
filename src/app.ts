72299e341090f4facdf57061f632d6c1c8d3ee36
adkshishir <adhikarishishir50@gmail.com>
2024-06-09 02:05:29 +0545

    update : schema for owner

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
+
 const app = express();
-const PORT = process.env.PORT||8080;
+const PORT = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
