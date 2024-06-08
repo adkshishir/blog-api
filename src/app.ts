a2c7bc32e4e5b0ab13c9c194bea2b855e17dfd63
adkshishir <adhikarishishir50@gmail.com>
2024-06-09 00:55:34 +0545

    add : app.ts

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+const app = express();
+const PORT = process.env.PORT||8080;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(PORT, () => {
+  console.log('Server is running on port: ' + PORT);
+});
+
+// kill server with ctrl + c
+process.on('SIGINT', () => {
+  console.log(' \n Shutting down server');
+  process.exit(0);
+});
