b44c1a382e36fb688bfcb38b80e7da7f08cfc517
adkshishir <adhikarishishir50@gmail.com>
2024-06-09 23:40:39 +0545

    update : static folder for images

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,17 @@
 import express from 'express';
 import routes from './routes';
+import * as path from 'path';
 const app = express();
 const PORT = process.env.PORT || 8080;
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use('/api/auth', routes.userRoutes);
 app.use('/api', routes.categoryRoutes);
+// Serve static files from the public directory
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.listen(PORT, () => {
+  console.log(path.join(__dirname, '../public'));
   console.log('Server is running on port: ' + PORT);
 });
 
