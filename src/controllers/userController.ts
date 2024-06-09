f55e478b70d9bec1c7cbf78b8ffcd8e0326b3e86
adkshishir <adhikarishishir50@gmail.com>
2024-06-09 16:58:50 +0545

    refactor : response

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,15 +1,18 @@
 import { NextFunction, Request, Response } from 'express';
 import User from '../services/User';
 import { userType } from '../types';
+import response from '../helpers/formateResponse';
 
 class UserController {
   async createUser(req: Request, res: Response, next: NextFunction) {
     try {
       const userData: userType = req.body;
       const user = await User.register(userData);
-      return res.status(201).json(user);
+      return res
+        .status(201)
+        .json(response(201, 'User created successfully', user));
     } catch (error) {
-      return res.status(400).json(error);
+      return res.status(400).json(response(400, 'User already exists'));
     }
   }
 
@@ -17,9 +20,12 @@ class UserController {
     try {
       const { email, password } = req.body;
       const user = await User.login({ email, password });
-      res.status(200).json(user);
-    } catch (error) {
-      return res.status(400).json(error);
+      res.status(200).json(response(200, 'User logged in successfully', user));
+    } catch (error: any) {
+      if (error.status === 404) {
+        return res.status(404).json(response(404, 'User not found'));
+      }
+      return res.status(400).json(response(400, error.message));
     }
   }
 }
