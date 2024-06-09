ac53a7e9b5a9c70403a23590709de1cbdada43c6
adkshishir <adhikarishishir50@gmail.com>
2024-06-09 12:19:19 +0545

    add : express json for body parser

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,9 @@
 import express from 'express';
+import routes from './routes';
 const app = express();
-const PORT = process.env.PORT||8080;
-
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+const PORT = process.env.PORT || 8080;
+app.use(express.json());
+app.use('/api/auth', routes.userRoutes);
 
 app.listen(PORT, () => {
   console.log('Server is running on port: ' + PORT);
