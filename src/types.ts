4f3e3e790cfd71b4c72807e56320956d7bde8321
adkshishir <adhikarishishir50@gmail.com>
2024-06-09 23:40:51 +0545

    add : category type

--- src/types.ts
+++ src/types.ts
@@ -8,3 +8,27 @@ export type userType = {
     emailVerifiedAt?: Date;
 }
 
+export type categoryType = {
+    name?: string;
+    description?: string;
+    slug?: string;
+    image?: {
+        url?: string
+        alt?: string
+    }
+    tags?: string[]
+    id?: number
+    seo?: {
+       metaTitle?: string
+       metaDescription?: string
+        metaKeywords?: string
+        canonical?: string
+       ogTitle?: string
+       ogDescription?: string
+       ogImage?: string
+       twitterTitle?: string
+       twitterDescription?: string
+       twitterImage?: string
+    }
+
+}
