b501a7aa12115116df2908b15837f5194a8482f7
adkshishir <adhikarishishir50@gmail.com>
2024-06-09 23:42:14 +0545

    update : category routes

--- src/routes/categoriesRoutes.ts
+++ src/routes/categoriesRoutes.ts
@@ -1,7 +1,30 @@
 import { Router } from 'express';
 import categoriesController from '../controllers/categoriesController';
+import { uploadImage } from '../libs/multer/uploadImage';
+import { validateCreateCategory } from '../validators/categoryValidator';
+import userMiddleware from '../middleware/userMiddleware';
 const router = Router();
 router.get('/categories', categoriesController.index);
+router.post(
+  '/categories',
+  userMiddleware.checkAdmin,
+  validateCreateCategory,
+  uploadImage,
+  categoriesController.create
+);
+router.get('/categories/:slug', categoriesController.showBySlug);
+router.get('/categories/id/:id', categoriesController.show);
+router.delete(
+  '/categories/:id',
+  userMiddleware.checkAdmin,
+  categoriesController.delete
+);
+router.patch(
+  '/categories/:id',
+  userMiddleware.checkAdmin,
+  uploadImage,
+  categoriesController.update
+);
 
 const categoryRoutes = router;
 export default categoryRoutes;
