26f58c3616fc63665e5e8de2f310a73b10c1c61c
adkshishir <adhikarishishir50@gmail.com>
2024-06-10 11:02:36 +0545

    add : tag services

---
+++ src/services/tag.ts
@@ -0,0 +1,140 @@
+import prisma from './prisma';
+
+class Tag {
+  async getAllTags() {
+    try {
+      const tags = await prisma.tag.findMany();
+      return tags;
+    } catch (error) {
+      throw error;
+    }
+  }
+  async getTag(id: number) {
+    try {
+      const tag = await prisma.tag.findUnique({
+        where: {
+          id,
+        },
+      });
+
+      if (!tag) {
+        throw {
+          status: 404,
+          message: 'Tag not found',
+        };
+      }
+      return tag;
+    } catch (error) {
+      throw error;
+    }
+  }
+  async getTagsBySlug(slug: string) {
+    try {
+      const tag = await prisma.tag.findFirst({
+        orderBy: {
+          createdAt: 'desc',
+        },
+        where: {
+          slug,
+        },
+      });
+
+      if (!tag) {
+        throw {
+          status: 404,
+          message: 'Tag not found',
+        };
+      }
+      return tag;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async createTag(data: any) {
+    try {
+      const imageUrl = process.env.BASE_URL + '/uploads/' + data.image?.url;
+      let image: any = undefined;
+      if (data?.image) {
+        image = {
+          create: {
+            url: imageUrl,
+            alt: data.image.alt,
+          },
+        };
+      }
+      const tag = await prisma.tag.create({
+        data: {
+          name: data.name,
+          slug: data.slug,
+          categoryId: data.categoryId,
+          description: data.description,
+          image: image,
+          seo: {
+            create: data.seo,
+          },
+        },
+      });
+      return tag;
+    } catch (error: any) {
+      if (error.code === 'P2002') {
+        throw {
+          status: 400,
+          message: 'Tag already exists',
+        };
+      }
+      throw { error };
+    }
+  }
+  async updateTag(id: number, data: any) {
+    try {
+      const imageUrl = process.env.BASE_URL + '/uploads/' + data.image?.url;
+      let image: any = undefined;
+      if (data?.image) {
+        image = {
+          create: {
+            url: imageUrl,
+            alt: data.image.alt,
+          },
+        };
+      }
+      const tag = await prisma.tag.update({
+        where: {
+          id,
+        },
+        data: {
+          name: data.name,
+          slug: data.slug,
+          categoryId: data.categoryId,
+          description: data.description,
+          image: image,
+          seo: {
+            update: data.seo,
+          },
+        },
+      });
+      return tag;
+    } catch (error: any) {
+      if (error.code === 'P2002') {
+        throw {
+          status: 400,
+          message: 'Tag already exists',
+        };
+      }
+      throw { error };
+    }
+  }
+
+  async deleteTag(id: number) {
+    try {
+      const tag = await prisma.tag.delete({
+        where: {
+          id,
+        },
+      });
+      return tag;
+    } catch (error) {
+      throw error;
+    }
+  }
+}
