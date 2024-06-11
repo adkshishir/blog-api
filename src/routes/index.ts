0ac4072c31c569ab27ff0f8bf9cc5af49f5e4413
adkshishir <adhikarishishir50@gmail.com>
2024-06-11 16:57:05 +0545

    add : post routes to index

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,7 +2,9 @@ import userRoutes from './userRoutes';
 import categoryRoutes from './categoriesRoutes';
 import tagRoutes from './tagsRoutes';
 import profileRoutes from './profileRoutes';
+import postRoutes from './postRoutes';
 export default {
+  postRoutes,
   userRoutes,
   categoryRoutes,
   tagRoutes,
