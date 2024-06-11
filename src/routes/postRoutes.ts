25fb391016090a0dd0a460f267db8af6b7d30f0f
adkshishir <adhikarishishir50@gmail.com>
2024-06-11 17:34:10 +0545

    add : upload.none() to fix middleware body null issue on create post

--- src/routes/postRoutes.ts
+++ src/routes/postRoutes.ts
@@ -3,13 +3,18 @@ const router = Router();
 import postController from '../controllers/postsController';
 import postValidator from '../validators/postValidator';
 import userMiddleware from '../middleware/userMiddleware';
+import { upload } from '../libs/multer/uploadImage';
 
 router.get('/posts', postController.index);
 router.get('/posts/:id', postController.show);
 router.get('/posts/slug/:slug', postController.showBySlug);
-router.post('/posts', userMiddleware.checkAdmin, postController.create);
+router.post(
+  '/posts',
+  userMiddleware.checkAdmin,
+  upload.none(),
+  postController.create
+);
 router.patch('/posts/:id', userMiddleware.checkAdmin, postController.update);
 router.delete('/posts/:id', userMiddleware.checkAdmin, postController.delete);
 const postRoutes = router;
 export default postRoutes;
- 
